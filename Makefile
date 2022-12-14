d94817d14ac1261026ea1e5f0f451d703554aa55
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-12-15 00:19:29 +0300

    upd naming (HeroImportance-> CharacterType) enum, feat separate enum from it, feat working seed for it.

--- Makefile
+++ Makefile
@@ -13,3 +13,13 @@ seed:
 # веб-интерфейс Prisma
 studio:
 	npx prisma studio
+
+# приводит схему призмы в красивый вид + добавляет недостающие связи:
+align:
+	npx prisma format
+
+# миграция схемы с дефолтным именем
+mig:
+	npx prisma migrate dev --name nextMigrate
+
+
---
+++ src/database/seeds/characters-seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+import { CharacterType} from "../../core/anime/types/character-type.enum";
+
+const prisma = new PrismaClient();
+
+async function characterSeed() {
+    console.log(`Start seeding characters...`);
+    await prisma.character.createMany({
+        skipDuplicates: true,
+        data: [
+            {
+                bucket_id: 'f2c57902-3981-406b-a9e6-1f15a62019a1',
+                character_name: 'Сикакунагай',
+                importance: CharacterType.PROTAGONIST,
+                description: 'этот парень был из тех, за кем летает стерх'
+            },
+            {
+                bucket_id: 'f2c57902-3981-406b-a9e6-1f15a62019a1',
+                character_name: 'КтоУгодноСан',
+                importance: CharacterType.ANTAGONIST,
+                description: 'главный антагонист Сикакунагай'
+            },
+        ],
+    });
+    console.log(`Seeding anime finished.`);
+}
+
+characterSeed()
+    .catch((e) => console.error(e))
+    .finally(async () => await prisma.$disconnect());
