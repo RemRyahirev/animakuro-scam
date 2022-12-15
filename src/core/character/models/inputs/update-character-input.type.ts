9e5bb7d42ab516ce2c82663e4360a21803f8dfda
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-12-15 22:53:28 +0300

    feat update-character-input.type.ts

---
+++ src/core/character/models/inputs/update-character-input.type.ts
@@ -0,0 +1,39 @@
+import { ArgsType, Field, ID } from 'type-graphql';
+import {
+    IsOptional,
+    IsString,
+    Length,
+} from 'class-validator';
+
+import {CharacterType} from "../../../../common/models/enums";
+
+@ArgsType()
+export class UpdateCharacterInputType {
+    @Field(() => ID)
+    id: string;
+
+    @IsOptional()
+    @IsString()
+    @Field(() => String, { nullable: true })
+    bucket_id?: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(1, 50)
+    @Field(() => String, { nullable: true })
+    character_name?: string;
+
+    @IsOptional()
+    @IsString()
+    @Field(() => CharacterType, {
+        nullable: true,
+        defaultValue: CharacterType.PROTAGONIST
+    })
+    importance?: CharacterType;
+
+    @IsOptional()
+    @IsString()
+    @Field(() => String, { nullable: true })
+    description?: string;
+
+}
