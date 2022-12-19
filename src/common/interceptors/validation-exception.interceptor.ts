b759deb0c2e7dd139e1bfa4708742ad896671101
Vitaly Kadyrov <vitaly.kadyrov@splzone.com>
2022-12-19 14:54:52 +0200

    prettier formatted

--- src/common/interceptors/validation-exception.interceptor.ts
+++ src/common/interceptors/validation-exception.interceptor.ts
@@ -1,28 +1,38 @@
-import { ArgumentValidationError, MiddlewareFn, ResolverData } from "type-graphql";
-import { ValidationError } from "class-validator";
-import { BaseResultsType } from "../models/results";
-import { CustomErrorType } from "../models/types";
+import {
+    ArgumentValidationError,
+    MiddlewareFn,
+    NextFn,
+    ResolverData,
+} from 'type-graphql';
+import { ValidationError } from 'class-validator';
+import { BaseResultsType } from '../models/results';
+import { CustomErrorType } from '../models/types';
 
-export const ValidationErrorInterceptor: MiddlewareFn<any> = async (resolverData: ResolverData<{}>, next) => {
+export const ValidationErrorInterceptor: MiddlewareFn<any> = async (
+    resolverData: ResolverData<{}>,
+    next: NextFn,
+) => {
     try {
         return await next();
     } catch (error: any) {
-        if(error && error instanceof ArgumentValidationError){
+        if (error && error instanceof ArgumentValidationError) {
             let errorsArray: CustomErrorType[] = [];
             error.validationErrors.map((error: ValidationError) => {
                 for (const key in error.constraints) {
                     errorsArray.push({
                         property: error.property,
-                        value: Array.isArray(error.value) ? 'empty array' : error.value,
+                        value: Array.isArray(error.value)
+                            ? 'empty array'
+                            : error.value,
                         reason: error.constraints[key],
                     });
                 }
             });
             return <BaseResultsType>{
                 success: false,
-                errors: errorsArray
+                errors: errorsArray,
             };
         }
         throw error;
     }
-}
+};
