ee7338a7d81b28de49b4dedf4187a96a2d0a8e44
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-12-22 20:11:15 +0300

    refractor WatchStatus in schema + feat watch-status.enum.ts

---
+++ src/common/models/enums/watch-status.enum.ts
@@ -0,0 +1,13 @@
+import { registerEnumType } from 'type-graphql';
+import 'reflect-metadata';
+
+export enum WatchStatus {
+    WATCHING = 'WATCHING',      // Currently watching
+    PLANNED = 'PLANNED',        // Planned to watch
+    COMPLETED = 'COMPLETED',    // renamed from SEEN. Finished watching
+    DROPPED = 'DROPPED',        // renamed from ABANDONED. Stopped watching before completing
+}
+
+registerEnumType(WatchStatus, {
+    name: 'WatchStatus',
+});
