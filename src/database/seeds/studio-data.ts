af640d20f96fd3977672452b640ad2e481f8a696
Vitaly Kadyrov <vitaly.kadyrov@splzone.com>
2022-12-23 17:49:32 +0200

    anime connect to studio

--- src/database/seeds/studio-data.ts
+++ src/database/seeds/studio-data.ts
@@ -1,10 +1,26 @@
-export const studioData = async () => [
-    {
-        id: 'ac9ab0e3-2d72-4e7c-9255-d1a20e49b113',
-        createdAt: new Date(Date.now()),
-        studio_name: 'Anime International',
-        rating: 3.2,
-        thumbnail: 'https://foo.bar.jpg',
-        // anime: animeArray,
-    },
-];
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const studioData = async () => {
+    const animeList = await prisma.anime.findMany();
+    return [
+        {
+            id: 'ac9ab0e3-2d72-4e7c-9255-d1a20e49b113',
+            createdAt: new Date(Date.now()),
+            studio_name: 'Anime International',
+            rating: 3.2,
+            thumbnail: 'https://foo.bar.jpg',
+            anime: {
+                connect: [
+                    {
+                        id: animeList[0].id
+                    },
+                    {
+                        id: animeList[1].id
+                    }
+                ]
+            },
+        },
+    ];
+};
