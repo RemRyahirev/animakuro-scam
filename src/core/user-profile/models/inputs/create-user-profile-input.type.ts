530eb55a2c7f7e22c2ac062850c843450bb93ea3
DIMANVAZ <DIMANVAZ04@GMAIL.COM>
2022-12-26 18:46:40 +0300

    feat create-user-profile-input.type.ts

---
+++ src/core/user-profile/models/inputs/create-user-profile-input.type.ts
@@ -0,0 +1,66 @@
+import {ArgsType, Field} from "type-graphql";
+import {User} from "../../../user/models/user.model";
+import {UserAnime} from "../../../user-anime/models/user-anime.model";
+import {ModeratorRoles, SubscribeTier} from "../../../../common/models/enums";
+import {IsBoolean, IsDate, IsOptional, IsString, IsUUID, Length} from "class-validator";
+
+
+@ArgsType()
+export class CreateUserProfileInputType{
+
+    @Field(() => User)
+    user: User;
+
+    @Field(() => UserAnime)
+    user_anime: UserAnime;
+
+    @IsString()
+    @Length(1,30)
+    @Field()
+    displayed_name: string;
+
+    @IsOptional()
+    @IsUUID()
+    @Field({nullable: true, description: 'internal CDN ID/uuid'})
+    profile_picture_id?: string;
+
+    @IsOptional()
+    @IsUUID()
+    @Field({nullable: true, description: 'internal CDN ID/uuid'})
+    banner_image?: string;
+
+    @IsOptional()
+    @IsString()
+    @Field({nullable: true})
+    about?: string
+
+    @IsOptional()
+    @IsString()
+    @Length(1,30)
+    @Field({nullable: true})
+    country?: string
+
+    @IsString()
+    @Length(1,30)
+    @Field()
+    language: string;
+
+    @IsDate()
+    @Field({defaultValue: Date.now()})
+    createdAt: Date;
+
+    @IsString()
+    @Field(() => SubscribeTier,
+        {defaultValue: SubscribeTier.FREE_ACCOUNT})
+    subscribe_tier: string;
+
+    @IsString()
+    @Field(() => ModeratorRoles,
+        {defaultValue: ModeratorRoles.VIEWER})
+    moderator_role: string;
+
+    @IsBoolean()
+    @Field({defaultValue: false})
+    isBlocked: boolean
+
+}
