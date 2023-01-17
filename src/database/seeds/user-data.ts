07b16748c55e212dbd925631dab9378182430d6f
KingMaxud <maxud754@gmail.com>
2023-01-17 19:46:11 +0300

    added avatar field to user-data seed

--- src/database/seeds/user-data.ts
+++ src/database/seeds/user-data.ts
@@ -8,6 +8,7 @@ export const userData = async () => [
         password: '$2b$10$zfTmralcT619fZHCl0Tml.CzzvDm0tti6qcewAOWMBGBwylIBa/x2', // password
         gender: Gender.MALE,
         is_email_confirmed: true,
+        avatar: '',
     },
     {
         id: '5b594675-a3c6-4e1b-acaa-2e612419b9e2',
@@ -16,6 +17,7 @@ export const userData = async () => [
         password: '$2b$10$zfTmralcT619fZHCl0Tml.CzzvDm0tti6qcewAOWMBGBwylIBa/x2', // password
         gender: Gender.MALE,
         is_email_confirmed: true,
+        avatar: '',
     },
     {
         id: '71d46841-94f0-4310-88c3-662b7fd817ec',
@@ -24,6 +26,7 @@ export const userData = async () => [
         password: '$2b$10$zfTmralcT619fZHCl0Tml.CzzvDm0tti6qcewAOWMBGBwylIBa/x2', // password
         gender: Gender.MALE,
         is_email_confirmed: true,
+        avatar: '',
     },
     {
         id: '03c6c54d-8597-43b8-a793-4fb36522cc61',
@@ -31,7 +34,8 @@ export const userData = async () => [
         username: 'Oleg',
         password: '$2b$10$UTBzDrug3h9HvegQbhg6UuEqg8Be8hj8JS2WD3AJl9cg7RdfWjfHi', // another-password
         gender: Gender.MALE,
-        is_email_confirmed: false
+        is_email_confirmed: false,
+        avatar: '',
     },
     {
         id: '4c323fe8-d35a-4e84-9f75-92f20fcec028',
@@ -39,7 +43,8 @@ export const userData = async () => [
         username: 'Irina',
         password: '$2b$10$LfmYoVBbTMMaRfR4VZRUw.9UGRla.UDJ0icOkj4LrpUJomMFdzlXm', // some-password
         gender: Gender.FEMALE,
-        is_email_confirmed: false
+        is_email_confirmed: false,
+        avatar: '',
     },
     {
         id: '2b2133d4-6973-4c15-88a6-8b8c19c96fab',
@@ -47,6 +52,7 @@ export const userData = async () => [
         username: 'Svetlana',
         password: '$2b$10$LfmYoVBbTMMaRfR4VZRUw.9UGRla.UDJ0icOkj4LrpUJomMFdzlXm', // some-password
         gender: Gender.FEMALE,
-        is_email_confirmed: false
+        is_email_confirmed: false,
+        avatar: '',
     },
 ];
