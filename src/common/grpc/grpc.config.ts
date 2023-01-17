e981b4a69ccf6c7d430d854c526ba7c77b3f9273
KingMaxud <maxud754@gmail.com>
2023-01-17 17:18:34 +0300

    added credentials

--- src/common/grpc/grpc.config.ts
+++ src/common/grpc/grpc.config.ts
@@ -1,12 +1,15 @@
 import { ClientOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { credentials } from "@grpc/grpc-js";
 
 export const grpcClientConfig: ClientOptions = {
     transport: Transport.GRPC,
+    // @ts-ignore
     options: {
         package: 'document',
         protoPath: join(__dirname, './document.proto'),
         url: process.env.GRPC_URL,
         loader: { keepCase: true },
+        credentials: credentials.createSsl(),
     },
 };
