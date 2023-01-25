c077daead95c47d047285499bf6a0fe29a1ff7e4
KingMaxud <maxud754@gmail.com>
2023-01-25 16:50:38 +0300

    added a few airingSchedule seeds

--- src/database/seeds/airing-schedule.ts
+++ src/database/seeds/airing-schedule.ts
@@ -1,3 +1,26 @@
 export const airingScheduleData = async () => [
-
+    {
+        airing_at: new Date("2023-01-21T12:00:00Z"),
+        name: "The Surprise That Leads to Tragedy",
+        episode: 1070,
+        anime_id: '5318851d-3348-4cf7-81a9-b08ccf505796',
+    },
+    {
+        airing_at: new Date("2023-01-28T12:00:00Z"),
+        name: "Yusaku Kudo's Detective Show (Part 1)",
+        episode: 1071,
+        anime_id: '5318851d-3348-4cf7-81a9-b08ccf505796',
+    },
+    {
+        airing_at: new Date("2023-01-24T12:00:00Z"),
+        name: "Talking About Love",
+        episode: 15,
+        anime_id: 'f4b8318d-55c0-4b84-a83e-42560006801d',
+    },
+    {
+        airing_at: new Date("2023-01-31T12:00:00Z"),
+        name: "Unknown name",
+        episode: 16,
+        anime_id: 'f4b8318d-55c0-4b84-a83e-42560006801d',
+    },
 ];
