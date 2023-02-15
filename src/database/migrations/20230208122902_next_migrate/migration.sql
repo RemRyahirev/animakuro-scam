4cc715a84cc1fda0412dc447f98ce6b260350535
STARGUS <stanislav.morr@gmail.com>
2023-02-15 06:57:44 +0300

    Откат соммита

--- src/database/migrations/20230208122902_next_migrate/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "IpList" (
-    "ip" TEXT NOT NULL,
-    "banTime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "IpList_ip_key" ON "IpList"("ip");
--- src/database/migrations/20230213110730_migration_name/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "FolderType" AS ENUM ('LOOKING', 'ABANDONED', 'PLANNED', 'VIEWED', 'DEFAULT');
-
--- AlterTable
-ALTER TABLE "user_folder" ADD COLUMN     "type" "FolderType" NOT NULL DEFAULT 'DEFAULT';
