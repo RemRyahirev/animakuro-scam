7aae6aacb4d473a528a752a67e72835109b14297
STARGUS <stanislav.morr@gmail.com>
2023-02-21 18:38:49 +0300

    Добавить новые дефолтные папки

--- src/common/models/enums/folder-type.enum.ts
+++ src/common/models/enums/folder-type.enum.ts
@@ -2,10 +2,12 @@ import { registerEnumType } from '@nestjs/graphql';
 import 'reflect-metadata';
 
 export enum FolderType {
-    LOOKING = 'LOOKING', //Смотрю
-    ABANDONED = 'ABANDONED', // Брошено
-    PLANNED = 'PLANNED', // Запланированно
-    VIEWED = 'VIEWED', // Просмотрено
+    WATCHING = 'WATCHING', //Смотрю
+    PLAN_TO_WATCH = 'PLAN_TO_WATCH', // Брошено
+    COMPLETED = 'COMPLETED', // Запланированно
+    REWATCHING = 'REWATCHING', // Просмотрено
+    PAUSED = 'PAUSED', // Просмотрено
+    DROPPED = 'DROPPED', // Просмотрено
     DEFAULT = 'DEFAULT', // Стандарный Фолдер
 }
 
