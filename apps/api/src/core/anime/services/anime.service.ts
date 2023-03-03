cea15a5e2eae22dc6a3c167b39108389a1ca6899
Andrey Ryakhovskiy <remryahirev@gmail.com>
2023-03-03 21:59:51 +0300

    refactoring consequences fix

--- apps/api/src/core/anime/services/anime.service.ts
+++ apps/api/src/core/anime/services/anime.service.ts
@@ -206,6 +206,7 @@ export class AnimeService {
                 id: profile_id,
             },
             select: {
+                id: true,
                 ...favouriteSelect,
             },
         });
